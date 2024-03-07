7f067e1e4a562529d0a94adad8a4c0b096ae0c0c
Chechere <cesartjmv@gmail.com>
2024-03-07 19:49:34 +0100

    Fixing log system and adding save log file button

--- main.py
+++ main.py
@@ -1,21 +1,41 @@
 from varfile import *
 
-import os, re, ssl, time, logging, traceback
+import os, re, ssl, time, traceback, logging
+
+from logging import LogRecord
 from threading import Thread
 
 from kivy.app import App, ObjectProperty
-from kivy.lang import Builder
 from kivy.config import Config
+from kivy.lang import Builder
 from kivy.uix.image import Image
 from kivy.uix.label import Label
 from kivy.uix.widget import Widget
 from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
+from kivy.logger import Logger, LoggerHistory
 
 from pytube import Playlist, YouTube
-import certifi
+import certifi        
 
-logger: logging.Logger
+class LogLabelHander(logging.Handler):
+    def __init__(self, label):
+        super(LogLabelHander, self).__init__()
+        self.label = label
+
+    def emit(self, record):
+        if not LOG_TAG in record.getMessage():
+            return
+
+        if record.levelno == logging.ERROR:    
+            self.label.color = ERROR_COLOR
+        elif record.levelno == logging.INFO:
+            self.label.color = INFO_COLOR
+        else:
+            self.label.color =  (1, 1, 1, 1)
+
+        self.label.text = record.getMessage().removeprefix(LOG_TAG)
+        print("HOla")          
 
 class AppGrid(Widget):
     info_label: Label = ObjectProperty(None)
@@ -25,106 +45,104 @@ class AppGrid(Widget):
     background_app: Image
 
     def __init__(self, **kwargs):
-        self.background_app = Image(source=BG_IMAGE).texture
-        self.background_app.wrap = "repeat"
-        self.background_app.uvsize = (8, -1)
+        self.background_app: Image = Image(source=BG_IMAGE).texture               
 
-        super().__init__(**kwargs)
+        super().__init__(**kwargs)  
 
-    def show_info(self, info: str, color: tuple, level: int, stacktrace: str):
-        self.info_label.color = color
-        self.info_label.text = info
-
-        logger.log(level, "\n".join([info, stacktrace]))
-
-    def show_error(self, info: str, stacktrace: str = ""):
-        self.show_info(info, ERROR_COLOR, logging.ERROR, stacktrace)
-
-    def show_debug(self, info: str, stacktrace: str = ""):
-        self.show_info(info, INFO_COLOR, logging.DEBUG, stacktrace)
+        Logger.addHandler(LogLabelHander(self.info_label))             
 
     def download_video(self, video):
         title = re.sub(REGEX_TITLE, "", video.title) + FILE_EXTENSION
 
-        self.show_debug("Downloading: " + title)
+        Logger.info(LOG_TAG + "Downloading: " + title)        
 
         if not os.path.exists(OUTPUT):
             os.mkdir(OUTPUT)
 
         try:
             video.streams.get_audio_only().download(
-                output_path=OUTPUT, filename=title, max_retries=3
-            )
-            self.show_debug(DOWNLOAD_SUCCESS)
-        except BaseException:
-            self.show_error(DOWNLOAD_ERROR, traceback.format_exc())
+                output_path=OUTPUT, 
+                filename=title, 
+                max_retries=3)           
+             
+            Logger.info(DOWNLOAD_SUCCESS)            
+        except BaseException:            
+            Logger.error(DOWNLOAD_ERROR, exc_info=traceback.format_exc())            
 
     def download_playlist(self, playlist):
         for video in playlist.videos:
             self.download_video(video)
             time.sleep(PLAYLIST_SLEEP_TIME)
 
-        self.show_debug(DOWNLOAD_PLAYLIST)
+        # self.show_info(DOWNLOAD_PLAYLIST, INFO_COLOR)
+        Logger.info(DOWNLOAD_PLAYLIST)        
 
-    def download(self, url):
-        self.info_label.text = ""
-
+    def download(self, url: str):
         try:
-            if self.isPlaylist(url):
+            if self.is_playlist(url):
                 self.download_playlist(Playlist(url))
             else:
                 self.download_video(YouTube(url))
         except BaseException:
-            self.show_error(URL_ERROR, traceback.format_exc())
+            Logger.error(URL_ERROR, exc_info=traceback.format_exc())            
 
         self.download_button.disabled = False
 
-    def isPlaylist(self, url: str) -> bool:
+    def is_playlist(self, url: str) -> bool:
         return url.find(URL_PLAYLIST_SUBSTR) != -1
 
     def button_click(self, instance):
         self.download_button.disabled = True
-        Thread(target=self.download, args=[self.text_input.text]).start()
 
+        Thread(
+            target=self.download, 
+            args=[self.text_input.text]).start()
 
+    def save_log_click(self, instance):        
+        if(not os.path.isdir(LOG_FOLDER)):            
+            Logger.info(CREATING_FOLDER_LOG)            
+            os.makedirs(LOG_FOLDER)                
+        
+        fh = logging.FileHandler(LOG_FILE_NAME_FORMAT)
+        fh.setFormatter(logging.Formatter(LOG_MESSAGE_FORMAT))
+        fh.setLevel(logging.DEBUG)        
+        
+        Logger.info(LOGGER_WRITTING_LOG_FILE)            
+
+        log:LogRecord                                
+        for log in reversed(LoggerHistory.history):
+            fh.emit(log)
+
+        Logger.info(LOG_SAVED)                    
+        fh.emit(LoggerHistory.history[0])
+            
 class YTAudioDownloader(App):
     def build(self):
         self.title = TITLE
-        self.root = Builder.load_file(LAYOUT_FILE)
+        self.icon = ICO_IMAGE        
+        self.root = Builder.load_file(LAYOUT_FILE)                
 
         return AppGrid()
 
-
-def set_logger() -> logging.Logger:
-    if(not os.path.isdir(LOG_FOLDER)):
-        os.makedirs(LOG_FOLDER)
-        
-    fh = logging.FileHandler(LOG_FILE_NAME_FORMAT)
-    fh.setFormatter(logging.Formatter(LOG_MESSAGE_FORMAT))
-    fh.setLevel(logging.DEBUG)
-
-    ch = logging.StreamHandler()
-    ch.setFormatter(logging.Formatter(LOG_MESSAGE_FORMAT))
-    ch.setLevel(logging.DEBUG)
-
-    logger = logging.getLogger("logger")
-    logger.setLevel(logging.DEBUG)
-    logger.addHandler(fh)
-    logger.addHandler(ch)
-
-    return logger
-
-
 if __name__ == "__main__":
-    logger = set_logger()
-
     ssl._create_default_https_context = ssl._create_stdlib_context
-    os.environ["SSL_CERT_FILE"] = certifi.where()
+    os.environ["SSL_CERT_FILE"] = certifi.where()    
 
-    Config.set("kivy", "window_icon", ICO_IMAGE)
-    Config.set("graphics", "resizable", "0")
+    Config.set("graphics", "resizable", False)        
 
     try:
-        YTAudioDownloader().run()
+        app: YTAudioDownloader = YTAudioDownloader()
+        app.run()
     except BaseException:
-        logger.error("\n".join([APP_ERROR, traceback.format_exc()]))
+        print("ERROR! ", APP_ERROR)
+        print(traceback.format_exc())
+
+        fh = logging.FileHandler(LOG_FILE_NAME_FORMAT)
+        fh.setFormatter(logging.Formatter(LOG_MESSAGE_FORMAT))
+        fh.setLevel(logging.DEBUG)   
+
+        fh.emit(LogRecord(level= logging.ERROR, name= APP_ERROR, msg= traceback.format_exc(), 
+                exc_info= None, 
+                pathname= None, 
+                args= None, 
+                lineno= None))
--- varfile.py
+++ varfile.py
@@ -1,20 +1,26 @@
 from datetime import datetime
 
 TITLE: str = "Youtube Audio Downloader"
-
 LOG_FOLDER: str = "./logs/"
 LOG_FILE_NAME_FORMAT: str = LOG_FOLDER + "{:%Y-%m-%d}.log".format(datetime.now())
 LOG_MESSAGE_FORMAT: str = "%(asctime)s [%(levelname)s]: %(message)s"
+CREATING_FOLDER_LOG: str = "Creating log folder."
+LOGGER_WRITTING_LOG_FILE: str = "Writting on log file."
+SAVING_LOG: str = "Saving log {0}/{1}"
+LOG_SAVED: str = "Log file saved!"
+LOG_TAG: str = "Program: "
+
 
 LAYOUT_FILE: str = "./layout.kv"
 
-ASSETS_FOLDER: str = "./assets/"
-BG_IMAGE: str = ASSETS_FOLDER + "background.jpeg"
-BTN_UP_IMAGE: str = ASSETS_FOLDER + "button.jpeg"
-BTN_DOWN_IMAGE: str = ASSETS_FOLDER + "button_down.jpeg"
-ICO_IMAGE: str = ASSETS_FOLDER + "icon.png"
-TITLE_IMAGE: str = ASSETS_FOLDER + "title.png"
-URL_LABEL_IMAGE: str = ASSETS_FOLDER + "url_label.png"
+BG_IMAGE: str = "./assets/background.jpeg"
+BTN_UP_IMAGE: str = "./assets/button.jpeg"
+BTN_DOWN_IMAGE: str = "./assets/button_down.jpeg"
+ICO_IMAGE: str = "./assets/icon.png"
+TITLE_IMAGE: str = "./assets/title.png"
+URL_LABEL_IMAGE: str = "./assets/url_label.png"
+LOG_BUTTON_UP_IMAGE: str = "./assets/log_button.png"
+LOG_BUTTON_DOWN_IMAGE: str = "./assets/log_button_down.png"
 
 ERROR_COLOR: tuple = (1, 0, 0, 1)
 INFO_COLOR: tuple = (0, 1, 0, 1)
@@ -28,7 +34,7 @@ URL_PLAYLIST_SUBSTR: str = "playlist?list="
 PLAYLIST_SLEEP_TIME: float = 1.5
 
 APP_ERROR: str = "Error while running app"
-URL_ERROR: str = "Error getting video or playlist. Please, check URL"
-DOWNLOAD_SUCCESS: str = "Download Success!"
-DOWNLOAD_ERROR: str = "Error downloading music"
-DOWNLOAD_PLAYLIST: str = "Playlist downloaded"
+URL_ERROR: str = LOG_TAG + "Error getting video or playlist. Please, check URL"
+DOWNLOAD_SUCCESS: str = LOG_TAG + "Download Success!"
+DOWNLOAD_ERROR: str = LOG_TAG + "Error downloading music"
+DOWNLOAD_PLAYLIST: str = LOG_TAG + "Playlist downloaded"
